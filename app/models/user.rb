101edb8fd3058224feeab6a44cf5bde7d1e70ace
Franz Josef <franz.josef.bruenner@gmail.com>
2011-11-12 17:33:32 +0100

    changed: user model, username password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :username, :presence => true, :format => { :with => /^\D\w{4,12}/ }
+  validates :password, :presence => true, :length => { :minimum => 6 }
 end
