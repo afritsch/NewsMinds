c4d05738af9571aac9c54a6ad8eb24f0354a5c2e
Franz Josef <franz.josef.bruenner@gmail.com>
2011-12-08 15:38:41 +0100

    renamed: app/view/posts/index.html.erb in app/view/posts/userPosts.html.erb and config/initializers/secret_token.rb reseted

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Qpt2a::Application.config.secret_token = 'cbbb7457ca33aa99aacd5c394ba4fe16ee9a7153ec031f9792901b915d54e4c6d4d6f79c10df65b4f9487b7ad7d6834e881a861776858916db9a3e791c0ae4e5'
