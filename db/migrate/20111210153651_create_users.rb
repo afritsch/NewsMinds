6cb96f0e6af936fa7053a458be8b40d3edfd94e7
Franz Josef <franz.josef.bruenner@gmail.com>
2011-12-10 18:29:01 +0100

    column added: in app/controller/user_controller votePower so that goods users have got more rights and model methode added: in app/model/user.rb loggedInUser

---
+++ db/migrate/20111210153651_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.integer :numberOfPosVotedPosts
+      t.integer :numberOfNegVotedPosts
+      t.integer :numberOfPosCreatedPosts
+      t.integer :numberOfNegCreatedPosts
+      t.integer :mind
+      t.integer :votePower
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
