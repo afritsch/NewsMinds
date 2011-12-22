accd2e8be89f73a74219083e5bef22d931485bd5
Franz Josef <franz.josef.bruenner@gmail.com>
2011-12-22 17:43:27 +0100

    changed: in app/model/user.rb through server error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :oauthable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
-         
   validates :username, :presence => true, :format => { :with => /^\D{1,1}\S{3,12}$/ }
   validates :password, :presence => true, :format => { :with => /^\S{6,15}$/ }
 
