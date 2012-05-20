0731e7a71e8b60b0a876b364048dbd707c452cbe
Franz Josef <franz.josef.bruenner@gmail.com>
2012-05-20 20:49:48 +0200

    capybara using_session in ui_test.rb

--- test/integration/ui_tests_test.rb
+++ test/integration/ui_tests_test.rb
@@ -25,54 +25,72 @@ class UiTestsTest < ActionDispatch::IntegrationTest
     DailyNews.destroy_all
   end
   
-  test "user is registering" do
-    #visit register_path #root_path
-    #fill_in 'username', :with => @dummy_user.username
-    #fill_in 'password', :with => @dummy_user.password
-    #fill_in 'facebookEmail', :with => @dummy_user.facebookEmail
+  test "user is registering with already used username" do
+    visit register_path #root_path
+    fill_in 'username', :with => @dummy_user.username
+    fill_in 'password', :with => "geheim2"
+    fill_in 'facebookEmail', :with => ""
     
-    #click_button ' Registrieren '
-    #assert_equal true, page.has_content?('Erfolgreich registriert')
+    click_button ' Registrieren '
+    assert_equal true, page.has_link?('Registrieren')
   end
   
-  test "user is trying to log in" do
-    visit root_path #root_path
+  test "user is registering with already used Facebook-Email" do
+    visit register_path #root_path
+    fill_in 'username', :with => "Mr.Test2"
+    fill_in 'password', :with => "geheim2"
+    fill_in 'facebookEmail', :with => @dummy_user.facebookEmail
+    
+    click_button ' Registrieren '
+    assert_equal true, page.has_content?('Username oder Facebook Email Adresse schon vorhanden!')
+  end
+  
+  test "user is trying to log in with correct userinfo" do # this test has to go through in ordner to set session[:id] for next tests
+    visit root_path
     fill_in 'username', :with => @dummy_user.username
     fill_in 'password', :with => @dummy_user.password
     
     click_button ' Login '
-    assert_equal true, page.has_content?('Erfolgreich eingeloggt')
+    assert_equal true, page.has_button?(' Logout ')
   end
   
   test "user has entered wrong username" do
-    #visit root_path #root_path
-    #fill_in 'username', :with => @dummy_user.username.to_s << "_"
-    #fill_in 'password', :with => @dummy_user.password
     
-    #click_button ' Login '
-    #assert_equal true, page.has_content?(' Login ')
+    Capybara.using_session("id") do
+      visit root_path
+      fill_in 'username', :with => @dummy_user.username.to_s << "_"
+      fill_in 'password', :with => @dummy_user.password
+    
+      click_button ' Login '
+      assert_equal true, page.has_content?('Falsche Eingabe')
+    end
     
   end
   
   test "user has entered wrong password" do
-    #visit root_path #root_path
-    #fill_in 'username', :with => @dummy_user.username
-    #fill_in 'password', :with => "_" << @dummy_user.password.to_s
+  
+    Capybara.using_session("id") do
+      visit root_path
+      fill_in 'username', :with => @dummy_user.username.to_s
+      fill_in 'password', :with => @dummy_user.password.to_s << "_"
     
-    #click_button ' Login '
-    #assert_equal true, page.has_content?(' Login ')
+      click_button ' Login '
+      assert_equal true, page.has_content?('Falsche Eingabe')
+    end
     
   end
   
-  test "user wants to change their profile" do 
+  test "user wants to change their password" do 
     
-    #visit profile_path
-    #fill_in 'oldPassword', :with => @password
-    #click_button ' Neues Passwort '
-    #fill_in 'user_password', :with => "geheim2"
-    #click_button 'user_submit'
+    #Capybara.using_session("id") do
+    #  visit profile_path
+     # fill_in 'oldPassword', :with => @password
+    #  click_button ' Neues Passwort '
+     # fill_in 'user_password', :with => "geheim2"
+      #click_button 'user_submit'
     
-    #page.has_content?('geheim2')
+      #assert_equal true, page.has_content?('geheim2')
+    #end
     
   end
   
